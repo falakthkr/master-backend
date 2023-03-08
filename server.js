62481ce5ef1bfbb3cea54079f3d989d01adf9a63
Falak Thackar <falak@hotmail.co.in>
2023-03-08 15:12:43 +0530

    updated server

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 var bodyParser = require('body-parser');
+const cors = require("cors");
 
 dotenv.config();
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// app.use(cookieParser());
+app.use(cors());
 
 const userRoute = require("./routes/users")
 
 app.use("/users", userRoute)
 
 mongoose.connect(
-    process.env.ATLAS_URI,
-    {
-        useNewUrlParser : true,
-        useUnifiedTopology : true,
+    process.env.ATLAS_URI.toString(), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     },
-    (err)=>{
-        if(err){
+    (err) => {
+        if (err) {
+            console.log(err)
             console.log("Error connecting the database")
-        }
-        else{
+        } else {
             console.log("Database successfully connected")
         }
     }
@@ -31,6 +33,6 @@ mongoose.connect(
 
 const port = process.env.PORT || 8000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`The server is up and running on ${port}!`)
 })
