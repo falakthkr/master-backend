3356d0974f6084de2e5bb884ca8eaa617697f6f3
falakthkr <falak@hotmail.co.in>
2023-05-21 15:03:11 +0530

    update: property model

--- models/estic/property.js
+++ models/estic/property.js
@@ -6,18 +6,11 @@ const propertySchema = new Schema(
     name: String,
     location: String,
     price: String,
-    bedrooms: Number,
-    bathrooms: Number,
-    external_size: String,
-    size: String,
     exterior_amenities: [
       {
         type: String,
       },
     ],
-    garden: String,
-    reference: String,
-    cooling_systems: [{ type: String }],
     description: String,
     rera_no: String,
     images: [
