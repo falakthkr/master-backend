ab55279110fcbbea1e30ba9af5c41fc4b284bfb0
falakthkr <falak@hotmail.co.in>
2023-05-21 14:00:53 +0530

    add Property func

--- controllers/estic/propertyController.js
+++ controllers/estic/propertyController.js
@@ -1,19 +1,58 @@
-const Property = require("../../models/estic/property")
+const Property = require("../../models/estic/property");
 
-const getProperties = (req,res) => {
-    Property.find()
-    .then((properties=>res.json(properties)))
-    .catch((err)=>res.status(400).json("Error: "+err))
-}
+const getProperties = (req, res) => {
+  Property.find()
+    .then((properties) => res.json(properties))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
 
-const getPropertyDetails = (req,res) => {
-    Property.findById(req.params.id)
-    .then((property)=>{
-        res.json(property);
+const getPropertyDetails = (req, res) => {
+  Property.findById(req.params.id)
+    .then((property) => {
+      res.json(property);
     })
-    .then(()=>res.json("Fetched property data successfully"))
-    .catch((err)=>res.status(400).json("Error: "+err))
-}
+    .then(() => res.json("Fetched property data successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
 
+const addProperty = (req, res) => {
+  const {
+    name,
+    location,
+    price,
+    bedrooms,
+    bathrooms,
+    external_size,
+    size,
+    exterior_amenities,
+    garden,
+    reference,
+    cooling_systems,
+    description,
+    rera_no,
+    images,
+  } = req.body;
+  const newProperty = new Property({
+    name,
+    location,
+    price,
+    bedrooms,
+    bathrooms,
+    external_size,
+    size,
+    exterior_amenities,
+    garden,
+    reference,
+    cooling_systems,
+    description,
+    rera_no,
+    images,
+  });
 
-module.exports = { getProperties, getPropertyDetails }
+  newProperty
+    .save()
+    .then(() => res.json("Property added successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
+
+module.exports = { getProperties, getPropertyDetails, addProperty };
